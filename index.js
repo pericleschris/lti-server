8880c204229214f255e76909b925d5a992b0835e
Pericles Christian <pericleschrstn@gmail.com>
2024-05-13 12:46:35 -0300

    fix(cookies): add mode and credentials flags

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ lti.setup(
       secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
       sameSite: "None", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
       domain: ".azurewebsites.net",
+      mode: cors,
+      credentials: "include",
     },
     // ltiaas: true,
     devMode: false, // Set DevMode to true if the testing platform is in a different domain and https is not being used
