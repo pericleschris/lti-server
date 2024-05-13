8b0e15940f31d54d6ac7c86b8f34e954357bf69c
Pericles Christian <pericleschrstn@gmail.com>
2024-05-13 14:40:49 -0300

    fix: set SameSite lax

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ lti.setup(
   {
     cookies: {
       secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: "None", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+      sameSite: "Lax", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
       domain: ".azurewebsites.net",
-      mode: "cors",
-      credentials: "include",
     },
     // ltiaas: true,
     devMode: false, // Set DevMode to true if the testing platform is in a different domain and https is not being used
