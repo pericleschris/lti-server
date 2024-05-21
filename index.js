11b9250ac01fb593249e387533d241693966a3ec
Pericles Christian <pericleschrstn@gmail.com>
2024-05-21 13:52:41 -0300

    fix: domain on cookies

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ lti.setup(
     cookies: {
       secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
       sameSite: "None", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
-      domain: ".azurewebsites.net",
+      domain: "azurewebsites.net",
     },
     devMode: false, // Set DevMode to true if the testing platform is in a different domain and https is not being used
     // ltiaas: true,
